5a93837d34f7384e3f3f5ec0f1a4014693bf401d
bmehder <bmehder@gmail.com>
2023-12-26 15:11:56 +0000

    refactored types

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,49 @@
 <script lang="ts">
-	// @ts-ignore
+	// @ts-ignore - nejQuery is not typed
 	import { see } from 'nejquery'
 
-	const pipe =
-		(...fns: any[]) =>
-		(arg: any) =>
-			fns.reduce((acc: any, fn) => fn(acc), arg)
+	// Types
+	type StrOrNum = string | number
+	type ReducerFunction = (acc: any, fn: Function) => Function
 
-	const map = (fn: any) => (xs: any[]) => xs.map(fn)
-	
-	const identity = <const T,>(x: T) => x
+	type Pipe = (...fns: Function[]) => (arg: any) => any // Is this the best that can be done?
+	type MapType = (fn: any) => (xs: any[]) => any[] // Best I could do, for now.
+	type Identity = <const T>(x: T) => T
 
-	const isDivisibleBy = (y: number) => (x: number) => x % y === 0
+	type NumToNumToBool = (y: number) => (x: number) => boolean
+	type NumToNum = (x: number) => number
+	type ArrayOfNumsToArrayOfNums = (xs: number[]) => number[]
+	type JoinBy = (str?: string) => (xs: StrOrNum[]) => string
 
-	const inc = (x: number) => x + 1
+	type IncrementArrayValues = (xs: number[]) => number[]
+	type CreateFizzBuzzEntries = (xs: number[]) => StrOrNum[]
+	type JoinArrayWithBreakTag = (xs: StrOrNum[]) => string
 
-	const getArrayKeys = (xs: number[]) => [...xs.keys()]
+	type FizzBuzzWords = 'Fizz' | 'Buzz' | 'FizzBuzz'
+	type FizzBuzzEntry = (x: number) => FizzBuzzWords | number
+	type FizzBuzz = (x: number) => string
 
-	const join =
-		(str = '') =>
-		(xs: (string | number)[]) =>
-			xs.join(str)
+	// Callback functions
+	const reducerFunction: ReducerFunction = (state, fn) => fn(state)
 
+	// Utility functions
+	// prettier-ignore
+	const pipe: Pipe = (...fns) => arg =>	fns.reduce(reducerFunction, arg)
+	const map: MapType = fn => xs => xs.map(fn)
+	const identity: Identity = x => x
+	const isDivisibleBy: NumToNumToBool = y => x => x % y === 0
+	const inc: NumToNum = x => x + 1
+	const getArrayOfKeys: ArrayOfNumsToArrayOfNums = xs => [...xs.keys()]
+	// prettier-ignore
+	const join: JoinBy = (str = '') => xs => xs.join(str)
+
+	// Partially applied functions
 	const isDivisibleBy15 = isDivisibleBy(15)
 	const isDivisibleBy5 = isDivisibleBy(5)
 	const isDivisibleBy3 = isDivisibleBy(3)
 
-	const createEntry = (x: number) => {
+	// Domain-specific callback function
+	const createEntry: FizzBuzzEntry = x => {
 		if (isDivisibleBy15(x)) return 'FizzBuzz'
 		if (isDivisibleBy5(x)) return 'Buzz'
 		if (isDivisibleBy3(x)) return 'Fizz'
@@ -34,13 +51,19 @@
 		return identity(x)
 	}
 
-	const getRangeOfNumbers = (x: number) => pipe(Array, getArrayKeys)(x) as number[]
-	const incrementValues = (xs: number[]) => map(inc)(xs) as number[]
-	const createEntries = (xs: number[]) => map(createEntry)(xs) as (string|number)[]
-	const joinWithBreakTag = (xs: (string|number)[]) => join('<br />')(xs)
+	// Domain-specific loop functions
+	const incrementArrayValues: IncrementArrayValues = map(inc)
+	const createFizzBuzzEntries: CreateFizzBuzzEntries = map(createEntry)
+	const joinArrayWithBreakTag: JoinArrayWithBreakTag = join('<br />')
 
-	const fizzBuzz = (x: number): string =>
-		pipe(getRangeOfNumbers, incrementValues, createEntries, see, joinWithBreakTag)(x)
+	// App function - the program in a nutshell
+	const fizzBuzz: FizzBuzz = pipe(
+		Array,
+		getArrayOfKeys,
+		incrementArrayValues,
+		createFizzBuzzEntries,
+		joinArrayWithBreakTag
+	)
 </script>
 
 <main>
